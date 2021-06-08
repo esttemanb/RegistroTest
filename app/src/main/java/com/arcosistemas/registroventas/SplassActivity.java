f32a6dc3b0a2844dc000c9e86940e45975799edd
esttemanb <ebb_8@hotmail.com>
2021-06-08 15:05:00 -0500

    Initial commit

---
+++ app/src/main/java/com/arcosistemas/registroventas/SplassActivity.java
@@ -0,0 +1,20 @@
+package com.arcosistemas.registroventas;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splass);
+
+        /*Intent intent = new Intent(SplassActivity.this, DashboardActivity.class);
+        startActivity(intent);
+        // close this activity
+        finish();*/
+    }
+}
